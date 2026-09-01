5b8eb0029de55babbbdeb8df08cbf3a07f6a209d
Fares Hegazy <fthegazy2010@gmail.com>
2026-09-01 14:28:02 -0400

    Scripts Cleanup

--- Scripts/Setup-Windows.bat
+++ Scripts/Setup-Windows.bat
@@ -13,10 +13,10 @@ REM --------------------------------------------------
 set "ROOT=%~dp0.."
 
 REM --------------------------------------------------
-REM Initialize / update Git submodules
+REM Initialize / update all Git submodules
 REM --------------------------------------------------
 
-echo [1/4] Initializing Git submodules...
+echo [1/3] Initializing Git submodules...
 
 git -C "%ROOT%" submodule update --init --recursive
 
@@ -31,16 +31,14 @@ echo Git submodules ready.
 echo.
 
 REM --------------------------------------------------
-REM Delete generated files/folders
+REM Delete generated build files
+REM
+REM .idea is intentionally NOT deleted.
+REM Rider's project settings should persist between
+REM regenerations.
 REM --------------------------------------------------
 
-echo [2/4] Cleaning generated files...
-
-REM .idea
-if exist "%ROOT%\.idea" (
-    echo Removing .idea...
-    rmdir /s /q "%ROOT%\.idea"
-)
+echo [2/3] Cleaning generated files...
 
 REM Binaries
 if exist "%ROOT%\Binaries" (
@@ -54,22 +52,25 @@ if exist "%ROOT%\Tagbound.sln" (
     del /f /q "%ROOT%\Tagbound.sln"
 )
 
-REM Visual Studio project files
+REM VertexEditor project
 if exist "%ROOT%\Vertex\VertexEditor.vcxproj" (
     echo Removing VertexEditor.vcxproj...
     del /f /q "%ROOT%\Vertex\VertexEditor.vcxproj"
 )
 
+REM VertexRuntime project
 if exist "%ROOT%\Vertex\VertexRuntime.vcxproj" (
     echo Removing VertexRuntime.vcxproj...
     del /f /q "%ROOT%\Vertex\VertexRuntime.vcxproj"
 )
 
+REM raylib project
 if exist "%ROOT%\Vertex\Vendor\raylib.vcxproj" (
     echo Removing raylib.vcxproj...
     del /f /q "%ROOT%\Vertex\Vendor\raylib.vcxproj"
 )
 
+REM Tagbound project
 if exist "%ROOT%\Tagbound\Tagbound.vcxproj" (
     echo Removing Tagbound.vcxproj...
     del /f /q "%ROOT%\Tagbound\Tagbound.vcxproj"
@@ -82,7 +83,7 @@ REM --------------------------------------------------
 REM Generate Visual Studio solution
 REM --------------------------------------------------
 
-echo [3/4] Generating Visual Studio solution...
+echo [3/3] Generating Visual Studio solution...
 
 cd /d "%ROOT%"
 
@@ -95,14 +96,10 @@ if errorlevel 1 (
     exit /b 1
 )
 
-echo Premake generation complete.
 echo.
-
-REM --------------------------------------------------
-REM Finish
-REM --------------------------------------------------
-
-echo [4/4] Setup complete.
+echo ========================================
+echo Setup complete!
+echo ========================================
 echo.
 echo Generated:
 echo   Tagbound.sln
